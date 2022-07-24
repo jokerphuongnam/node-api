d7ee50b9f60918d29993ad2cdcc60f08509d2b53
Pham P. Nam <phuongnamp99@gmail.com>
2022-07-24 17:37:17 +0700

    Fix remove searchNotes

--- services/notesService/index.js
+++ services/notesService/index.js
@@ -2,12 +2,10 @@ const getNotes = require('./getNotes')
 const insertNote = require('./insertNote')
 const updateNote = require('./updateNote')
 const deleteNote = require('./deleteNote')
-const searchNotes = require('./searchNotes')
 
 module.exports = {
     getNotes,
     insertNote,
     updateNote,
-    deleteNote,
-    searchNotes
+    deleteNote
 }
